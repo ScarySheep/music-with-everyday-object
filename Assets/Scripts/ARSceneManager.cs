31a820aae7f98a14faa101536030e37e693e7f24
James Lee <b05502067@ntu.edu.tw>
2022-04-15 02:45:28 -0400

    add scene manager

---
+++ Assets/Scripts/ARSceneManager.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class ARSceneManager : MonoBehaviour
+{
+    public GameObject musicMenu;
+    public GameObject objectToPlace;
+    public GameObject ARCursor;
+    public Text debugText;
+
+    private DemoCursor demoCursor;
+    private List<GameObject> objectPlaced = new List<GameObject>();
+    private enum State
+    {
+        Placing,
+        SettingSound
+    }
+    private State state = State.Placing;
+
+    void Start()
+    {
+        debugText.text = "State placing";
+        demoCursor = ARCursor.GetComponent<DemoCursor>();
+    }
+
+
+    void Update()
+    {
+        switch (state)
+        {
+            case State.Placing:
+                {
+                    //if point is on a plane and there is a on touch down
+                    if (demoCursor.hits.Count > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+                    {
+                        GameObject obj = Instantiate(objectToPlace, demoCursor.transform.position, demoCursor.transform.rotation); // place at cursor pos
+                        objectPlaced.Add(obj);
+                        musicMenu.SetActive(true);
+                        debugText.text = "State setting sound";
+                        state = State.SettingSound; //switch to setting sound mode after placing the block
+                    }
+                }
+                break;
+            default: break;
+        }
+    }
+
+
+    public void CloseMenu()
+    {
+        musicMenu.SetActive(false);
+        state = State.Placing;
+    }
+}
--- Assets/Scripts/DemoCursor.cs
+++ Assets/Scripts/DemoCursor.cs
@@ -1,74 +1,33 @@
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
-using UnityEngine.EventSystems;
 
 /* CREDIT: from the tutorial "Get Started with AR in Unity in 6 minutes!" */
 
 public class DemoCursor : MonoBehaviour
 {
     public GameObject cursorChildObject;
-    public GameObject objectToPlace;
-    public GameObject musicMenu;
-    //private Canvas musicMenuCanvas;
     public ARRaycastManager rayCastManager;
-    public Text debugText;
-    private List<GameObject> objectPlaced = new List<GameObject>();
-
+    public List<ARRaycastHit> hits = new List<ARRaycastHit>();
     public bool useCursor = true;
 
-    //private bool closedMenu = false;
-
-    private enum State
-    {
-        Placing,
-        SettingSound
-    }
-    private State state = State.Placing;
-    // Start is called before the first frame update
     void Start()
     {
         cursorChildObject.SetActive(useCursor);
-        debugText.text = "State placing";
-        //musicMenuCanvas = musicMenu.GetComponent<Canvas>();
-        musicMenu.SetActive(false);
     }
 
-    // Update is called once per frame
     void Update()
     {
-
         if (useCursor)
         {
             UpdateCursor();
-        }
-
-        switch (state)
-        {
-            case State.Placing:
-                {
-                    List<ARRaycastHit> hits = new List<ARRaycastHit>();
-                    rayCastManager.Raycast(Input.GetTouch(0).position, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
-                    if (hits.Count > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
-                    {
-                        GameObject obj = Instantiate(objectToPlace, transform.position, transform.rotation); // place at cursor pos
-                        objectPlaced.Add(obj);
-                        //musicMenuCanvas.enabled = true;
-                        musicMenu.SetActive(true);
-                        state = State.SettingSound; //switch to setting sound mode after placing the block
-                        debugText.text = "State setting sound";
-                    }
-                }
-                break;
-            default: break;
         }
     }
 
     void UpdateCursor()
     {
         Vector2 screenPosition = Camera.main.ViewportToScreenPoint(new Vector2(0.5f, 0.5f));
-        List<ARRaycastHit> hits = new List<ARRaycastHit>();
+        hits = new List<ARRaycastHit>();
         rayCastManager.Raycast(screenPosition, hits, UnityEngine.XR.ARSubsystems.TrackableType.Planes);
 
         if (hits.Count > 0)
@@ -76,12 +35,5 @@ public class DemoCursor : MonoBehaviour
             transform.position = hits[0].pose.position;
             transform.rotation = hits[0].pose.rotation;
         }
-    }
-
-    public void CloseMenu()
-    {
-        //closedMenu = true;
-        musicMenu.SetActive(false);
-        state = State.Placing;
     }
 }
